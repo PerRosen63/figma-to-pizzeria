eff4edca3be85341685a9a35ac7bac946ed6bb50
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-04-12 15:13:54 +0200

    init array with food data

---
+++ menuData.js
@@ -0,0 +1,15 @@
+const menu = [
+    {
+        category: 'Pizzor',
+        items: [
+            { name: 'Margarita', description: 'Tomatsås, mozzarella, oregano' },
+            { name: 'Hawaii', description: 'Tomatsås, ost, skinka, ananas' },
+
+        ]
+    },
+    {
+        category: 'Souvlaki',
+        items:
+        { name: 'Kycklingsouvlaki', description: 'Marinerad kyckling med pitabröd och tzatsiki'},
+    }
+]
