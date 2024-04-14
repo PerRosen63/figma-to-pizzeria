da5d56d79ab6821ab5695072ad61746f104fc75c
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-04-14 09:36:24 +0200

    :tada: menuData array finished

--- menuData.js
+++ menuData.js
@@ -2,14 +2,60 @@ const menu = [
     {
         category: 'Pizzor',
         items: [
-            { name: 'Margarita', description: 'Tomatsås, mozzarella, oregano' },
-            { name: 'Hawaii', description: 'Tomatsås, ost, skinka, ananas' },
+            { name: 'Vesuvio', description: 'ost, skinka'},
+            { name: 'Bolognese', description: 'ost, köttfärssås'},
+            { name: 'Capricciosa', description: 'ost, skinka, champinjoner'},
+            { name: 'Margarita', description: 'ost' },
+            { name: 'Pescatore', description: 'ost, tonfisk'},
+            { name: 'Hawaii', description: 'ost, skinka, ananas' },
+            { name: 'Marinara', description: 'ost, musslor, lök'},
+            { name: 'La Maffia', description: 'ost, bacon, lök'}
 
         ]
     },
     {
         category: 'Souvlaki',
-        items:
-        { name: 'Kycklingsouvlaki', description: 'Marinerad kyckling med pitabröd och tzatsiki'},
+        items: [
+        { name: 'Grillspett - Fläsk', description: 'Fläskarré, tomati, paprika, lök, tzatziki' },
+        { name: 'Grillspett - Kyckling', description: 'Kycklingfilé, tomat, paprika, lök, tzatziki' },
+        { name: 'Grillspett - Lamm', description: 'Lammfilé, tomat, paprika, lök, tzatziki' },
+        { name: 'Grillspett - Vegetarisk', description: 'tomat, paprika, lök, majs, champinjoner, tztziki'}
+        ]
+    },
+    {
+        category: 'Hamburgare',
+        items: [
+            { name: 'Cheese Burger ', description: 'Hellmans mayo, ketchup, rödlök, saltgurka, cheddarost' },
+            { name: 'Chicken Burger', description: 'Krispig kycklingfilé, Hellmans mayo, krispsallad, tomat, lök' },
+            { name: 'Chilimayo Burger', description: 'BBQ-sås, chilimayo, tomat, krispsallad, picklad rödlök, cheddarost' },
+            { name: 'Sourcream & Onion Burger', description: 'Rostad lök, karamelliserad lök, cheddarost, Sourcream & Onion dressing' }
+        ]
+    },
+    {
+        category: 'Kebab',
+        items: [
+            { name: 'Kebab i pitabröd', description: 'kebabkött, sallad, tomat, gurka, lök, feferoni, valfri sås' },
+            { name: 'Kebabrulle', description: 'kebabkött, sallad, tomat, gurka, lök feferoni, valfri sås' },
+            { name: 'Kebabtallrik', description: 'kebabkött, sallad, tomat, gurka, lök, feferoni, pommes, valfri sås' },
+            { name: 'Kebabpizza', description: 'kebabkött, ost, sallad tomat, gurka, lök, feferoni, valfri sås'}
+        ]
+    },
+    {
+        category: 'Grillat',
+        items: [
+            { name: 'Ryggbiff med kappa', description: 'serveras med grillade grönsaker, bearnaise, rödvinssås och pommes' },
+            { name: 'Oxfilé', description: 'serveras med grillade grönsaker, bearnaise, rödvinssås och pommes' },
+            { name: 'Köttfest Xtra', description: 'Tre olika grillade styckdetaljer serveras med grillade grönsaker, bearnaise, rödvinssås och pommes' },
+            { name: 'Entrecote', description: 'serveras med grillade grönsaker, bearnaise, rödvinssås och pommes' }
+        ]
+    },
+    {
+        category: 'Falafel',
+        items: [
+            { name: 'Falafelrulle', description: 'falafel, sallad, tomat, gurka, lök, feferoni, valfri sås' },
+            { name: 'Falafeltallrik', description: 'falafel, sallad, tomat, gurka, lök, feferoni, pommes, valfri sås'}
+        ]
     }
-]
+];
+
+export default menu;
