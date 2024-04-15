f0d05a7b82707a784bb93ddf25ff69c2c25f94c9
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-04-15 13:04:08 +0200

    initial commit showDishes

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import './scss/style.css'
-
+import { showDishes } from './showDishes.js';
 import { createCookieBar } from './cookie.js';
 
 document.addEventListener('DOMContentLoaded', function () {
+  showDishes;
   let hideCookieBar = createCookieBar();
 })
---
+++ showDishes.js
@@ -0,0 +1,59 @@
+import menu from "./menuData.js";
+
+const mobileDishes = ["Kebabpizza", "Sourcream & Onion Burger"];
+const tabletDishes = [
+  "Kebabpizza",
+  "Sourcream & Onion Burger",
+  "Köttfest Xtra",
+  "Grillspett - Kyckling",
+  "Falafelrulle",
+  "Grillspett - Vegetarisk",
+];
+const desktopDishes = [
+  "Kebabpizza",
+  "Sourcream & Onion Burger",
+  "Köttfest Xtra",
+  "Grillspett - Kyckling",
+  "Falafelrulle",
+  "Grillspett - Vegetarisk",
+];
+
+export function showDishes() {
+  let recommendedDishesDiv = document.getElementById("recommendedDishes");
+
+  function appendDishes(dishes) {
+    dishes.forEach(function (dishName) {
+      let dish = menu.find((item) => item.name === dishName);
+      if (dish) {
+        let dishContainer = createDishContainer(dish);
+        recommendedDishesDiv.appendChild(dishContainer);
+      }
+    });
+  }
+
+  // Append differend number of dishes based on platform
+  if (window.innerWidth < 768) {
+    appendDishes(mobileDishes);
+  } else if (window.innerWidth >= 768 && window.innerWidth < 1024) {
+    appendDishes(tabletDishes);
+  } else {
+    appendDishes(desktopDishes);
+  }
+}
+
+function createDishContainer(dish) {
+    let dishContainer = document.createElement('div');
+    dishContainer.classList.add('dish');
+
+    let dishTitle = document.createElement('h2');
+    dishTitle.textContent = dish.name;
+
+    let dishDescription = document.createElement('p');
+    dishDescription.textContent = dish.description;
+
+    dishContainer.appendChild(dishTitle);
+    dishContainer.appendChild(dishDescription);
+
+    return dishContainer;
+}
+document.addEventListener('DOMContentLoaded', showDishes);
