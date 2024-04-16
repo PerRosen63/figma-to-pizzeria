062760435c23e4cc7b51499d24ab7ca2909b1a36
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-04-16 20:13:58 +0200

    :sparkles: script remove disabled, sttyling form

--- main.js
+++ main.js
@@ -4,4 +4,12 @@ import { createCookieBar } from './cookie.js';
 
 document.addEventListener('DOMContentLoaded', function () {
   let hideCookieBar = createCookieBar();
-})
+});
+
+// Remove disabled from submit btn
+const submitBtn = document.querySelector('#submitBtn');
+const addressField = document.querySelector('#addressField');
+
+addressField.addEventListener('input', function() {
+submitBtn.removeAttribute('disabled', '')
+});
